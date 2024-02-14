c28ba5c83d48decadd6408a9a92df43a8921bc07
davidnaranjo91 <157336156+davidnaranjo91@users.noreply.github.com>
2024-02-13 21:41:07 -0500

    Add files via upload

---
+++ NumerosPrimos/NPrimos.Logic/NumPrimos.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPrimos.Logic
+{
+    public class NumPrimos
+    {
+
+        //atributos
+        private bool [] array;
+              
+        
+
+        //constructor
+        public NumPrimos(int num)
+        {
+            array = new bool [num];
+            int N = num +1 ;
+            for (int i = 2; i <= N; i++)
+            {
+                array[i] = true;
+            }
+
+            for (int j = 2; j * j <= N; j++)
+            {
+                if (array[j] == true)
+                {
+                    for(int i = j * j; i <= N; i += j)
+                    {
+                        array[i] = false;
+                    }
+                }
+            }
+
+            int c = 0;
+            for(int i = 2; i <= N; i++)
+            {
+                if (array[i])
+                {
+                    c++;
+                }
+            }
+
+     
+        }
+
+        public static int[] NumPrimos(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NumerosPrimos/NPrimos.UX/Program.cs
@@ -0,0 +1,9 @@
+﻿using NPrimos.Logic;
+
+
+var NumeroPrimo = new NumPrimos(10);
+Console.WriteLine("ingrese el numero hasta el que desee que se genere");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] primos = NumPrimos.NumPrimos(n);
+Console.WriteLine("Los numeros primos son");
+Console.WriteLine(primos);
